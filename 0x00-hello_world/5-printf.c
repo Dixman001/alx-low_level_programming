f4594dd31f4eb62e502234d3e5524e3201bd2553
Dixman001 <dixmaneagle@gmail.com>
2022-07-02 11:24:17 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  * main -> this is the Entry point for printing
- * printimg a statement
- * using the betty style
+ * 
  * return: Always 0 (success)
  */
-int main(void) /**main - Entry point*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
