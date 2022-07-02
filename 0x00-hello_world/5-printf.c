b9685c43e7fe6b1dfc84641ea4337238f44fa119
Dixman001 <dixmaneagle@gmail.com>
2022-07-02 07:49:37 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * printimg a statement
+ * using the betty style
+ */
+int main(void);
+	
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
