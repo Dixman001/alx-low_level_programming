461834881df8ccf5744580b20d27c4836e8808c2
Dixman001 <dixmaneagle@gmail.com>
2022-07-02 11:45:13 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
