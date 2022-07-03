159a8c0be51949e93957e0471783414f5ad85b57
Dixman001 <dixmaneagle@gmail.com>
2022-07-03 12:10:25 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main (void)
 {
 	char low;
 
-	for(low = 'a'; low <= 'z'; low++)
+	for(low='a'; low<='z'; low++)
 		putchar(low);
-	for(low = 'A'; low <= 'Z'; hi++)
+	for(low='A'; low<='Z'; hi++)
 		putchar(low);
 		printf('\n');
 	return(0);
