3d9c982a0b29d76edf33f8af29143ba8b0fb871c
Dixman001 <dixmaneagle@gmail.com>
2022-07-03 14:25:55 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ *
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	int z;
+
+
+	for (z = 0; z < 10; z++)
+
+		putchar (z);
+
+	putchar ('\n');
+
+	return (0);
+}
