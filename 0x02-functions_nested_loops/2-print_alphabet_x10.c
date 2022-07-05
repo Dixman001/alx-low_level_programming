22e5f20b9d2378da2403687ea9eed9c072950ae2
Dixman001 <dixmaneagle@gmail.com>
2022-07-05 14:40:12 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char l
-	int x
+	int l;
+	int x;
 
 	for (x = 0; x <= 10; x++)
 	{
 		for (l = 'a'; l <= 'z'; l++)
+		{
 			_putchar (l);
+		}
 	}
 	putchar ('\n')
 }
