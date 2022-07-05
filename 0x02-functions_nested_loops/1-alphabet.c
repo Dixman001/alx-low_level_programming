46f4af0ff4f40a31fbf829981eba827a29a4fc0f
Dixman001 <dixmaneagle@gmail.com>
2022-07-05 12:07:58 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * print_alphabet - Entry point
+ * Description: Print alphabet in lowcase
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{	char low;
+	for (low = 'a', low <= 'z', low++)
+		putchar (low);
+
+		putchar ('\n');
+
+	return (o);
+}
