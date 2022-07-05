a85b90cdb1dced7b845a7d87a00a96255548317d
Dixman001 <dixmaneagle@gmail.com>
2022-07-05 16:32:51 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: Compute absol;ute value of integer
+ * @n: AN integer
+ * Return:Value returned in n
+ */
+int _abs(int)
+{
+	if (n < 0)
+	n = -n;
+	return (n);
+}
