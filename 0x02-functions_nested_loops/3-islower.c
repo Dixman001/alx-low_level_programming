1e336d0b7f2307301f65eebeff26fd8ddbff758b
Dixman001 <dixmaneagle@gmail.com>
2022-07-05 15:40:03 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - Entry point
  * Description: Check for lowercase character
  * @c: a character to search for
- * Return: value reiurned 0 or 1 as the case may be 
+ * Return: value reiurned 0 or 1 as the case may be
  */
 int _islower(int c)
 {
