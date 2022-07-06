60e1ad4a1ed39f880e48ba076f8e7027a73ede3c
Dixman001 <dixmaneagle@gmail.com>
2022-07-06 09:35:59 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,19 +5,32 @@
  */
 void times_table(void)
 {
-	int a, b, c;
-
-	b = 9;
-
-
-	for (a = '0'; a <= '9'; a++)
+	int rone, cone, d;
+	
+	for (rone = 0; rone <= 9; rone++)
 	{
-		c = ((a * b) + 1);
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
-		_putchar (c);
-		_putchar (',');
-		_putchar (' ');
-		_putchar ('\n');
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' '_;
+			}
+		}
+		_putchar('\n');
 	}
-
 }
