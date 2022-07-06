b097f18beca6effe26e6db94bdfc772f34abbd40
Dixman001 <dixmaneagle@gmail.com>
2022-07-06 07:38:07 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,24 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * jack_bauer - Entry point
+ * description:print time from 0
+ * @h: integer
+ * @m: integer
  */
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h <= 24; h++)
 	{
 		for (m = 0; m <= 60; m++)
 		{
-			_putchar (m);
+			_putchar ((h / 10) + '0');
+			_putchar ((h % 10) + '0');
 			_putchar (':');
+			_putchar ((m / 10) + '0');
+			_putchar ((m % 10) + '0');
+			_putchar ('\n');
 		}
-		_putchar (h);
-		_putchar ('\n');
 	}
 }
