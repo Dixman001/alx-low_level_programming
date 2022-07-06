08c1eea42499620c66ee4944ef71746a61205e5f
Dixman001 <dixmaneagle@gmail.com>
2022-07-06 13:41:52 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * add - Entry point
- * @c: calue 3
- * @a: value 1
- * @b: value 2
- * Return: value returned is equal to c
+ * @c: sum of a and b
+ * @a: int to add with b
+ * @b: int to add with a
+ * Return: the sum of a and b
  */
 int add(int a, int b)
 {
