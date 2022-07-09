3f944204b417bcddb6ef2bae6e438b0e9c495c66
Dixman001 <dixmaneagle@gmail.com>
2022-07-09 14:52:52 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks for upper case
+ * @c: a declared character
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+	char c;
+	if (c > 'A' && c < 'Z')
+		isupper (c);
+		return (1);
+	else
+		return (0);
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+these are tasks for more functions and nested loops
