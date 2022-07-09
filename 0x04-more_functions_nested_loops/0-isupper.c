eae6c9cf16490ae9c45aa70638b4b2dee5edc349
Dixman001 <dixmaneagle@gmail.com>
2022-07-09 14:55:17 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,8 @@
  * @c: a declared character
  * Return: 1 if c is uppercase and 0 if otherwise
  */
+
 int _isupper(int c)
-	char c;
-	if (c > 'A' && c < 'Z')
-		isupper (c);
-		return (1);
-	else
-		return (0);
+{
+	return (c > 'A' && c < 'Z');
+}
