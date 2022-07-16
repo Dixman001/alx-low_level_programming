18fffd49727c91ca8273df7b8ce672e6d68504ea
Dixman001 <dixmaneagle@gmail.com>
2022-07-16 07:50:33 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int a;
 
-	while (s1[a] != '\0' || s2[a] != '\0')
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
 		if (s1[a] != s2[a])
 			return (s1[a] - s2[a]);
-		a++
 	}
 	return (0);
 }
