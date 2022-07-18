50c12d7c64a881bca9f57d0b0ed029cff41f802d
Dixman001 <dixmaneagle@gmail.com>
2022-07-18 15:12:43 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
-	while (i < n)
+
+	for (i = 0; i < n; i++)
 	{
-		src[i] = dest[i];
-		i++;
+		dest[i] = src[i];
 	}
-	return (dest)
+	return (dest);
 }
