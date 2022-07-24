68a20fde988707303bc4be3b8a9d5656fdb2733c
Dixman001 <dixmaneagle@gmail.com>
2022-07-24 13:33:30 -0700

    task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Ibezim Dixon");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 
 	return (0);
 }
