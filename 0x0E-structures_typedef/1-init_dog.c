edd65ff22f31a20f8826bf037ba3a4d16df8720e
Dixman001 <dixmaneagle@gmail.com>
2022-08-02 04:40:11 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,11 +14,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
-
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
+	{
 		return (NULL);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
