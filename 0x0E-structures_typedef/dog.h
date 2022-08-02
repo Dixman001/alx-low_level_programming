079bd9e2f9145dc35b255e4b8d38fa6bea3a7c6c
Dixman001 <dixmaneagle@gmail.com>
2022-08-02 03:42:12 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a data type that saves variables
+ * @name: the dog description
+ * @age: how old is the dog
+ * @owner: legal guardian of dog
+ */
+
 struct dog
 {
 	char *name;
