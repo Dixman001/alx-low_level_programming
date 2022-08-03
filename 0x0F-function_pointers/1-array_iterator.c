ea899e412e1d1606f4ff195386a37c400415877e
Dixman001 <dixmaneagle@gmail.com>
2022-08-03 13:42:19 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 {
 	unsigned int i;
 
-	if (array == NULL)
-		return;
-
-	if (action == NULL)
+	if (!array || !action)
 		return;
 
 	for (i = 0; i < size; i++)
