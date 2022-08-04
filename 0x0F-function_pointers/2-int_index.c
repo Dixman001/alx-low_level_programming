45f342c0462d8af88daa16d636f2b142e424b546
Dixman001 <dixmaneagle@gmail.com>
2022-08-03 17:34:06 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,14 +7,14 @@
  * @array: an array of int
  * @cmp: a pointer to array
  * @size: number of element
- * Return: a negative vaalue (-1) success
+ * Return: -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!size || size <= 0)
+	if (array ==NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
