d35350caf9e6125a80f126476613b35dd3a62f75
Dixman001 <dixmaneagle@gmail.com>
2022-08-11 08:50:28 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/*
+ * list_len - returns the numbers of elements in list_t
+ * @h: a pointer to struct
+ *
+ * Return: the number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		if (len < h->str)
+			len++;
+			h = h->next;
+	}
+	return (len);
+}
