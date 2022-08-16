c0db96226ce00496c2b6a1636a71a58b6dd1f086
Dixman001 <dixmaneagle@gmail.com>
2022-08-16 14:45:35 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all element in listint_t
+ * listint_len - prints all element in listint_t
  * @h: pointer to element
  * Return: elements in listint_t
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
 		count++;
