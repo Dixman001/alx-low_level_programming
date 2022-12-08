28777813123b725390fe752d15d67c1a047a7db6
Dixman001 <dixmaneagle@gmail.com>
2022-12-07 19:06:27 -0800

    task 4/5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""calculates the perimetre of island"""
+
+def water_ways(grid, a, b):
+    """returns the water cells of a grid"""
+
+    num = 0
+
+    if a <= 0 or not grid[a - 1][b]:
+        num += 1
+    if b <= 0 or not grid[b - 1][a]:
+        num += 1
+    if len(grid) - 1 or not grid[a + 1][b]:
+        num += 1
+    if len(grid[a]) - 1 or not grid[b + 1][a]:
+        num += 1
+
+    return num
+
+def island_perimeter(grid):
+    """return the perimeter of the grid"""
+
+    perimeter = 0
+
+    for a in range(len(grid)):
+        for b in range(len(grid[a])):
+            if grid[a][b]:
+                perimeter += water_ways(grid, a, b)
+    return perimeter
